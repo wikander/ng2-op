2ec155e7d56c1e550a74ef41fe2570ef8b258ff7
Fredrik Forsberg <fredrik.forsberg@omegapoint.se>
2016-05-13 15:50:16 +0200

    detailed view now aligned right of mobs list

--- app/mobs.component.ts
+++ app/mobs.component.ts
@@ -13,14 +13,38 @@ import { MobViewDetailsComponent } from './mob-viewdetails.component';
         [class.selected]="mob === selectedMob"
         class="mdl-list__item" >
         <span class="mdl-list__item-primary-content">
-          {{mob.name}} <a class="mdl-navigation__link" [routerLink]="['/mob/edit']">edit</a>
+          <span class="mob-link mdl-navigation__link">{{mob.name}}</span><a class="mdl-navigation__link" [routerLink]="['/mob/edit']">edit</a>
         </span>
       </li>
     </ul>
     <mob-viewdetails [mob]="theMob"></mob-viewdetails>
     `,
     directives: [ROUTER_DIRECTIVES, MobViewDetailsComponent],
-    providers: []
+    providers: [],
+    styles: [`
+      .mob-link {
+        cursor: pointer;
+        cursor: hand;
+        margin-right: 15px;
+      }
+
+      .mdl-list {
+        min-width: 200px;
+        flex: 0 1 auto
+      }
+
+      mob-viewdetails {
+        margin-top: 20px;
+        min-width: 600px;
+        flex: 0 1 auto
+      }
+
+      :host {
+        display: flex;
+        flex-direction: row;
+        justify-content: flex-start;
+      }
+    `]
 })
 export class MobsComponent implements OnActivate {
   constructor(private _mobService: MobService) {
