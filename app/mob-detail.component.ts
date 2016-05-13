bb01b7818094f07a711ecc0637e665189c0eaff2
Jonas Stendahl <jonas.stendahl@outlook.com>
2016-05-13 16:04:31 +0200

    delete mobber

--- app/mob-detail.component.ts
+++ app/mob-detail.component.ts
@@ -28,6 +28,7 @@ declare var componentHandler: any;
                   <i class="material-icons mdl-list__item-icon">person</i>
                   {{ mobber.name }}
               </span>
+              <button (click)="onDeleteMobber(mobber.order)"><i class="material-icons">delete</i></button>
             </li>
           </ul>
         </div>
@@ -66,5 +67,9 @@ export class MobDetailComponent implements AfterViewInit {
                    .subscribe(
                      mob => console.log(mob),
                      error =>  this.errorMessage = <any>error);
+  }
+  
+  onDeleteMobber(order: number){
+    this.mob.mobbers = this.mob.mobbers.filter(mobber => mobber.order != order);
   }
 }
